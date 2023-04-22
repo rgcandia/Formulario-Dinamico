d4dcf2263d05513dcdd5a730360fb261773308b3
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-04-22 10:07:21 -0300

    se realiza un css del componente PrintForm

--- src/components/PrintForm/PrintForm.jsx
+++ src/components/PrintForm/PrintForm.jsx
@@ -1,10 +1,13 @@
 import Nav from '../Nav/Nav.jsx'
+import style from './PrintForm.module.css'
 import { useSelector } from 'react-redux'; 
 const PrintForm = ()=>{
 const {form} = useSelector(state=>state.data)
     return (
     <div>
         <Nav/>
+        <div  className={style.form}>
+
         <br/>
         <h3>Visualización de tu formulario</h3>
         {form.map(e=>{
@@ -13,6 +16,7 @@ const {form} = useSelector(state=>state.data)
 
             </div>
         })}
+        </div>
         
     </div>
 )
